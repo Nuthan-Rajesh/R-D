7ae35ae2e463a1f5256ff31235564b3def16c081
Rajesh Neelakantam <92101730+Nuthan-Rajesh@users.noreply.github.com>
2023-03-28 11:28:29 +0530

    Add files via upload

---
+++ App/routes/users.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+
+// custom middleware to check auth state
+function isAuthenticated(req, res, next) {
+    if (!req.session.isAuthenticated) {
+        return res.redirect('/auth/signin'); // redirect to sign-in route
+    }
+
+    next();
+};
+
+router.get('/id',
+    isAuthenticated, // check if user is authenticated
+    async function (req, res, next) {
+        res.render('id', { idTokenClaims: req.session.account.idTokenClaims });
+    }
+);
+
+
+module.exports = router;
